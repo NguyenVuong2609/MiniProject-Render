eb5d64147e216cff1e75485901d959417e6a7a73
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2022-12-12 17:09:28 +0700

    1212

--- addProduct.js
+++ addProduct.js
@@ -5,23 +5,22 @@ function showHeaderAP() {
 }
 function showBodyAP() {
     let array =JSON.parse(localStorage.getItem("listProduct"));
-    console.log(array);
   let data = `<table>
   <tr>
       <td><label for="">Product's Name:</label></td>
-      <td><input type="text" placeholder="Product's name" class="inputInfo"></td>
+      <td><input type="text" placeholder="Product's name" class="inputInfo" id="nameProduct"></td>
   </tr>
   <tr>
       <td><label for="">Price:</label></td>
-      <td><input type="number" placeholder="Price" class="inputInfo"></td>
+      <td><input type="number" placeholder="Price" class="inputInfo" id="priceProduct"></td>
   </tr>
   <tr>
       <td><label for="">Picture:</label></td>
-      <td><input type="text" placeholder="Link" class="inputInfo"></td>
+      <td><input type="text" placeholder="Link" class="inputInfo" id="imgProduct"></td>
   </tr>
   <tr>
       <td><label for="">ID:</label></td>
-      <td id="productID">${array.length + 1}</td>
+      <td id="productID"></td>
   </tr>
   <tr class="button">
       <td><input type="button" value="Add" id="AddProduct"></td>
@@ -37,7 +36,22 @@ showBodyAP();
 function addProduct () {
     let btnAdd = document.getElementById("AddProduct");
     let btnDelete = document.getElementById("DeleteProduct");
+    let array = JSON.parse(localStorage.getItem('listProduct'));
     btnAdd.addEventListener("click",()=> {
-        
+      let name = document.getElementById("nameProduct");
+      let price = document.getElementById("priceProduct");
+      let img = document.getElementById("imgProduct");
+        array.push({
+          name: name.value,
+          price: parseInt(price.value),
+          img: img.value,
+          id: array.length + 1,
+          quantity: 0
+        })
+        name.value = "";
+        price.value = "";
+        img.value = "";
+        document.getElementById("productID").innerHTML = array.length + 1
+        console.log(array);
     });
 }
